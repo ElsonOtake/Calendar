ca49cb74b8cd1ae4c1460fe36f6c491288a9297e
Elson Otake <elsonotake@yahoo.com.br>
2023-12-28 18:31:21 -0300

    Add lib folder to modules path

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Calendar
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths += %W(#{config.root}/lib)
     config.beginning_of_week = :sunday
   end
 end
